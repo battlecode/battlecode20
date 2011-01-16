ac14c49afb99580e9a346e01a03112da07572dba
aibolit <aibolit@mit.edu>
2011-01-15 22:18:55 -0500

    ?

--- src/main/examplefuncsplayer/RobotPlayer.java
+++ src/main/examplefuncsplayer/RobotPlayer.java
@@ -5,47 +5,49 @@ import static battlecode.common.GameConstants.*;
 
 public class RobotPlayer implements Runnable {
 
-	private final RobotController myRC;
+    private final RobotController myRC;
 
     public RobotPlayer(RobotController rc) {
         myRC = rc;
     }
 
-	public void run() {
-		ComponentController [] components = myRC.newComponents();
-		System.out.println(java.util.Arrays.toString(components));
-		System.out.flush();
-		if(myRC.getChassis()==Chassis.BUILDING)
-			runBuilder((MovementController)components[0],(BuilderController)components[2]);
-		else
-			runMotor((MovementController)components[0]);
-	}
+    public void run() {
+        ComponentController[] components = myRC.newComponents();
+        System.out.println(java.util.Arrays.toString(components));
+        System.out.flush();
+        if (myRC.getChassis() == Chassis.BUILDING) {
+            runBuilder((MovementController) components[0], (BuilderController) components[2]);
+        } else {
+            runMotor((MovementController) components[0]);
+        }
+    }
 
-	public void testit(MovementController m) {
-		m.withinRange(myRC.getLocation());
-	}
+    public void testit(MovementController m) {
+        m.withinRange(myRC.getLocation());
+    }
 
-	public void runBuilder(MovementController motor, BuilderController builder) {
-	
-		while (true) {
+    public void runBuilder(MovementController motor, BuilderController builder) {
+
+        while (true) {
             try {
 
-				myRC.yield();
+                myRC.yield();
 
-				if(!motor.canMove(myRC.getDirection()))
-					motor.setDirection(myRC.getDirection().rotateRight());
-				else if(myRC.getTeamResources()>=2*Chassis.LIGHT.cost)
-					builder.build(Chassis.LIGHT,myRC.getLocation().add(myRC.getDirection()));
+                if (!motor.canMove(myRC.getDirection())) {
+                    motor.setDirection(myRC.getDirection().rotateRight());
+                } else if (myRC.getTeamResources() >= 2 * Chassis.LIGHT.cost) {
+                    builder.build(Chassis.LIGHT, myRC.getLocation().add(myRC.getDirection()));
+                }
 
             } catch (Exception e) {
                 System.out.println("caught exception:");
                 e.printStackTrace();
             }
         }
-	}
+    }
 
     public void runMotor(MovementController motor) {
-        
+
         while (true) {
             try {
                 /*** beginning of main loop ***/
