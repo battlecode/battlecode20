06a22517c26e8002f7717e16f18ceaacc0833343
Sherry Wu <shewu19@gmail.com>
2011-12-29 10:29:27 -0500

    copied example player to new directory

---
+++ src/main/shewuplayer/RobotPlayer.java
@@ -0,0 +1,38 @@
+package shewuplayer;
+
+import battlecode.common.*;
+import static battlecode.common.GameConstants.*;
+
+public class RobotPlayer implements Runnable {
+
+    private final RobotController myRC;
+
+    public RobotPlayer(RobotController rc) {
+        myRC = rc;
+    }
+
+    public void run() {
+        //System.out.println("STARTING");
+        while (true) {
+            try {
+                /*** beginning of main loop ***/
+                while (myRC.isMovementActive()) {
+                    myRC.yield();
+                }
+
+                if (myRC.canMove(myRC.getDirection())) {
+                    //System.out.println("about to move");
+                    myRC.moveForward();
+                } else {
+                    myRC.setDirection(myRC.getDirection().rotateRight());
+                }
+                myRC.yield();
+
+                /*** end of main loop ***/
+            } catch (Exception e) {
+                System.out.println("caught exception:");
+                e.printStackTrace();
+            }
+        }
+    }
+}
