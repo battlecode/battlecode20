fc1b02bdfe0d00834b2df9bad7f56d393efe0fbf
David Lu <uldivad9@mit.edu>
2014-01-05 15:06:35 -0500

    added examplefuncsplayer2

---
+++ src/main/examplefuncsplayer2/RobotPlayer.java
@@ -0,0 +1,85 @@
+package examplefuncsplayer2;
+
+import battlecode.common.Direction;
+import battlecode.common.GameConstants;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import battlecode.common.*;
+import java.util.*;
+
+public class RobotPlayer {
+	static Random rand;
+	
+	public static void run(RobotController rc) {
+		rand = new Random();
+		Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
+		
+		while(true) {
+			if (rc.getType() == RobotType.HQ) {
+				try {					
+					//Check if a robot is spawnable and spawn one if it is
+					if (rc.isActive() && rc.senseRobotCount() <= 25) {
+						Direction toEnemy = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+						if (rc.senseObjectAtLocation(rc.getLocation().add(toEnemy)) == null) {
+							rc.spawn(toEnemy);
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("HQ Exception");
+				}
+			}
+			
+			if (rc.getType() == RobotType.SOLDIER) {
+				try {
+					if (rc.isActive()) {
+						int action = (rc.getRobot().getID()*rand.nextInt(101) + 50)%101;
+						//Construct a PASTR
+						if (action < 1 && rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) > 2) {
+							rc.construct(RobotType.PASTR);
+						} else if (action < 2 && rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) > 2) {
+							rc.construct(RobotType.NOISETOWER);
+						//Attack a random nearby enemy
+						} else if (action < 30) {
+							Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class,10,rc.getTeam().opponent());
+							if (nearbyEnemies.length > 0) {
+								RobotInfo robotInfo = rc.senseRobotInfo(nearbyEnemies[0]);
+								rc.attackSquare(robotInfo.location);
+							}
+						//Move in a random direction
+						} else if (action < 80) {
+							Direction moveDirection = directions[rand.nextInt(8)];
+							if (rc.canMove(moveDirection)) {
+								rc.move(moveDirection);
+							}
+						//Sneak towards the enemy
+						} else {
+							Direction toEnemy = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+							if (rc.canMove(toEnemy)) {
+								rc.sneak(toEnemy);
+							}
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("Soldier Exception");
+				}
+			}
+			
+			if (rc.getType() == RobotType.NOISETOWER) {
+				try{
+					if (rc.isActive()) {
+						int action = rand.nextInt(2);
+						if (action == 0) {
+							rc.attackSquare(rc.getLocation().add(Direction.NORTH));
+						} else {
+							rc.attackSquareLight(rc.getLocation().add(Direction.SOUTH));
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("NOISETOWER Exception");
+				}
+			}
+			
+			rc.yield();
+		}
+	}
+}
