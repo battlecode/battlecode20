6e1d05d01055e8f38474e2e7319828ca0fff77f0
Alex X Chen <axcqwerty@gmail.com>
2014-08-15 16:09:39 -0400

    Add alextestplayer to avoid merge conflicts on examplefuncs

---
+++ src/main/alextestplayer/RobotPlayer.java
@@ -0,0 +1,62 @@
+package alextestplayer;
+
+import battlecode.common.Direction;
+import battlecode.common.GameConstants;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import battlecode.common.*;
+import java.util.*;
+
+public class RobotPlayer {
+	static Random rand;
+	
+	public static void run(RobotController rc) {
+		rand = new Random();
+		Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
+		
+		while(true) {
+			if (rc.getType() == RobotType.HQ) {
+				try {					
+					//Check if a robot is spawnable and spawn one if it is
+					if (rc.isActive() && rc.senseRobotCount() < 25) {
+						Direction toEnemy = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+						if (rc.senseObjectAtLocation(rc.getLocation().add(toEnemy)) == null) {
+							rc.spawn(toEnemy);
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("HQ Exception");
+				}
+			}
+			
+			if (rc.getType() == RobotType.SOLDIER) {
+				try {
+					if (rc.isActive()) {
+						int action = (rc.getRobot().getID()*rand.nextInt(101) + 50)%101;
+						//Mine
+						if (action < 20 && rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) > 2) {
+                            rc.mine();
+						//Attack a random nearby enemy
+						} else if (action < 50) {
+							Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class,10,rc.getTeam().opponent());
+							if (nearbyEnemies.length > 0) {
+								RobotInfo robotInfo = rc.senseRobotInfo(nearbyEnemies[0]);
+								rc.attackSquare(robotInfo.location);
+							}
+						//Move in a random direction
+						} else if (action < 100) {
+							Direction moveDirection = directions[rand.nextInt(8)];
+							if (rc.canMove(moveDirection)) {
+								rc.move(moveDirection);
+							}
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("Soldier Exception");
+				}
+			}
+			
+			rc.yield();
+		}
+	}
+}
