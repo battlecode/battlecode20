a4dc68db60c95b65677bc910c1a51a8bbd935be3
David Lu <uldivad9@mit.edu>
2014-11-07 10:46:22 -0500

    examnplefuncsupgrades

--- src/main/examplefuncsplayer/RobotPlayer.java
+++ src/main/examplefuncsplayer/RobotPlayer.java
@@ -4,14 +4,18 @@ import battlecode.common.*;
 import java.util.*;
 
 public class RobotPlayer {
-	static Random rand = new Random();
+	static RobotController rc;
+	static Random rand;
 	static Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
 	
-	public static void run(RobotController rc) {
-        MapLocation enemyLoc = rc.senseEnemyHQLocation();
+	public static void run(RobotController tomatojuice) {
+		rc = tomatojuice;
+        rand = new Random(rc.getRobot().getID());
+		MapLocation enemyLoc = rc.senseEnemyHQLocation();
         Direction lastDirection = null;
 		Team myTeam = rc.getTeam();
 		Team enemyTeam = myTeam.opponent();
+		Robot myRobots[];
 
 		while(true) {
             try {
@@ -24,14 +28,27 @@ public class RobotPlayer {
 
 			if (rc.getType() == RobotType.HQ) {
 				try {					
-					//Check if a robot is spawnable and spawn one if it is
-					if (rc.canMove() && rc.getTeamOre() >= 100) {
-                        Direction spawndir;
+					int fate = rand.nextInt(10000);
+					myRobots = rc.senseNearbyGameObjects(Robot.class, 999999, myTeam);
+					int numFurbies = 0;
+					for (Robot r : myRobots) {
+						if (rc.senseRobotInfo(r).type == RobotType.FURBY) {
+							numFurbies++;
+						}
+					}
+					if (rc.canMove() && rc.getTeamOre() >= 100 && fate < Math.pow(1.2,12-numFurbies)*10000) {
+                        
+						trySpawn(directions[rand.nextInt(8)], RobotType.FURBY);
+						
+						/*
+						Direction spawndir;
 						if (enemyLoc != null) {
 							spawndir = rc.getLocation().directionTo(enemyLoc);
 						} else {
 							spawndir = directions[rand.nextInt(8)];
 						}
+						
+						
 						int offsetIndex = 0;
 						int[] offsets = {0,1,-1,2,-2,3,-3,4};
 						int spawndirint = directionToInt(spawndir);
@@ -43,17 +60,11 @@ public class RobotPlayer {
 							System.out.println("creep blocked");
 						} else {
 							rc.spawn(directions[(spawndirint+offsets[offsetIndex]+8)%8], RobotType.FURBY);
+							
+							//give the robot some supplies
 						}
+						*/
 					}
-				} catch (Exception e) {
-					System.out.println("HQ Exception");
-                    e.printStackTrace();
-				}
-			}
-			if (rc.getType() == RobotType.BARRACKS) {
-				try {					
-					//Check if a robot is spawnable and spawn one if it is
-					
 				} catch (Exception e) {
 					System.out.println("HQ Exception");
                     e.printStackTrace();
@@ -64,7 +75,7 @@ public class RobotPlayer {
 				try {					
                     rc.transferSuppliesToHQ();
 				} catch (Exception e) {
-					System.out.println("supplydepot exception");
+					System.out.println("Supplydepot exception");
                     e.printStackTrace();
 				}
 			}
@@ -73,7 +84,7 @@ public class RobotPlayer {
                 try {					
 					
 				} catch (Exception e) {
-					System.out.println("trainingfield Exception");
+					System.out.println("Trainingfield Exception");
                     e.printStackTrace();
 				}
             }
@@ -82,7 +93,7 @@ public class RobotPlayer {
                 try {
                     
 				} catch (Exception e) {
-					System.out.println("miner Exception");
+					System.out.println("Miner Exception");
                     e.printStackTrace();
 				}
             }
@@ -104,15 +115,15 @@ public class RobotPlayer {
 					}
 					if (rc.canMove()) {
 						int fate = rand.nextInt(1000);
-						
-						if (fate < 500) {
+						if (fate < 10 && rc.getTeamOre() >= 250) {
+							tryBuild(directions[rand.nextInt(8)],RobotType.BARRACKS);
+						} else if (fate < 600) {
 							rc.mine();
 						} else if (fate < 900) {
 							Direction movedir = directions[rand.nextInt(8)];
-							
-							tryMove(rc, movedir);
+							tryMove(movedir);
 						} else {
-							tryMove(rc, rc.senseHQLocation().directionTo(rc.getLocation()));
+							tryMove(rc.senseHQLocation().directionTo(rc.getLocation()));
 						}
 					}
 				} catch (Exception e) {
@@ -123,17 +134,31 @@ public class RobotPlayer {
 
             if (rc.getType() == RobotType.BARRACKS) {
 				try {					
+					myRobots = rc.senseNearbyGameObjects(Robot.class, 999999, myTeam);
+					int numSoldiers = 0;
+					int numBashers = 0;
+					for (Robot r : myRobots) {
+						RobotType type = rc.senseRobotInfo(r).type;
+						if (type == RobotType.SOLDIER) {
+							numSoldiers++;
+						} else if (type == RobotType.BASHER) {
+							numBashers++;
+						}
+					}
+					if (rc.canMove() && rc.getTeamOre() >= 50) {
+						trySpawn(directions[rand.nextInt(8)], RobotType.SOLDIER);
+					}
 				} catch (Exception e) {
 					System.out.println("Barracks Exception");
                     e.printStackTrace();
 				}
-            }
+			}
 
 			rc.yield();
 		}
 	}
 	
-	static void tryMove(RobotController rc, Direction d) throws GameActionException {
+	static void tryMove(Direction d) throws GameActionException {
 		int offsetIndex = 0;
 		int[] offsets = {0,1,-1,2,-2};
 		int dirint = directionToInt(d);
@@ -143,6 +168,32 @@ public class RobotPlayer {
 		}
 		if (offsetIndex < 5) {
 			rc.move(directions[(dirint+offsets[offsetIndex]+8)%8]);
+		}
+	}
+	
+	static void trySpawn(Direction d, RobotType type) throws GameActionException {
+		int offsetIndex = 0;
+		int[] offsets = {0,1,-1,2,-2,3,-3,4};
+		int dirint = directionToInt(d);
+		boolean blocked = false;
+		while (offsetIndex < 8 && !rc.canMove(directions[(dirint+offsets[offsetIndex]+8)%8])) {
+			offsetIndex++;
+		}
+		if (offsetIndex < 8) {
+			rc.spawn(directions[(dirint+offsets[offsetIndex]+8)%8], type);
+		}
+	}
+	
+	static void tryBuild(Direction d, RobotType type) throws GameActionException {
+		int offsetIndex = 0;
+		int[] offsets = {0,1,-1,2,-2,3,-3,4};
+		int dirint = directionToInt(d);
+		boolean blocked = false;
+		while (offsetIndex < 8 && !rc.canMove(directions[(dirint+offsets[offsetIndex]+8)%8])) {
+			offsetIndex++;
+		}
+		if (offsetIndex < 8) {
+			rc.build(directions[(dirint+offsets[offsetIndex]+8)%8], type);
 		}
 	}
 	
