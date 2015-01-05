453023675cec827ecfacd011739e4d978f0968e4
Alex X Chen <axcqwerty@gmail.com>
2015-01-05 12:39:49 -0500

    Basic scalaplayer

---
+++ src/main/scalaplayer/RobotPlayer.scala
@@ -0,0 +1,20 @@
+package scalaplayer
+
+import battlecode.common._
+
+object RobotPlayer {
+
+    def run(myRC : RobotController) {
+        while(true) {
+            try {
+                myRC.`yield`()
+            } catch {
+                case e : Exception => {
+                    println("caught exception:")
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+}
