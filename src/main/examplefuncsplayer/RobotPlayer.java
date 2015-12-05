a680cd618d279aa55bc0b773b453156294f92f40
James Gilles <jhgilles@mit.edu>
2015-12-05 18:48:54 -0500

    Update to match movecontrol branch

--- src/main/examplefuncsplayer/RobotPlayer.java
+++ src/main/examplefuncsplayer/RobotPlayer.java
@@ -1,24 +1,21 @@
 package examplefuncsplayer;
 
 import battlecode.common.*;
-import java.util.*;
 
 public class RobotPlayer {
 	public static void run(RobotController rc) {
-        
+
         if (rc.getType() == RobotType.ARCHON) {
             try {
-            
-                // #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON #ARCHON
-                System.out.println(rc.getTeamOre());
+                System.out.println(rc.getTeamParts());
                 System.out.println("Building started");
                 rc.build(Direction.SOUTH_EAST, RobotType.GUARD);
-                System.out.println(rc.getTeamOre());
+                System.out.println(rc.getTeamParts());
                 for (int i=0; i<50; i++) {
-                    rc.yield();
+                    Clock.yield();
                 }
                 rc.build(Direction.SOUTH, RobotType.SOLDIER);
-                System.out.println(rc.getTeamOre());
+                System.out.println(rc.getTeamParts());
             } catch (Exception e) {
                 System.out.println("Archon initialization exception: " + e.getMessage());
                 e.printStackTrace();
@@ -26,14 +23,13 @@ public class RobotPlayer {
             
             while (true) {
                 try {
-                    rc.yield();
+                    Clock.yield();
                 } catch (Exception e) {
                     System.out.println("Archon exception: " + e.getMessage());
                     e.printStackTrace();
                 }
             }
         } else if (rc.getType() == RobotType.SOLDIER) {
-            // #SOLDIER #SOLDIER #SOLDIER #SOLDIER #SOLDIER #SOLDIER #SOLDIER #SOLDIER #SOLDIER #SOLDIER #SOLDIER
             try {
                 
             } catch (Exception e) {
@@ -47,14 +43,13 @@ public class RobotPlayer {
                     if (rc.canAttackLocation(hqloc) && rc.isWeaponReady()) {
                         rc.attackLocation(hqloc);
                     }
-                    rc.yield();
+                    Clock.yield();
                 } catch (Exception e) {
                     System.out.println("Soldier exception: " + e.getMessage());
                     e.printStackTrace();
                 }
             }
         } else {
-            // #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER #OTHER
             try {
                 
             } catch (Exception e) {
@@ -68,7 +63,7 @@ public class RobotPlayer {
                     if (rc.canAttackLocation(hqloc) && rc.isWeaponReady()) {
                         rc.attackLocation(hqloc);
                     }
-                    rc.yield();
+                    Clock.yield();
                 } catch (Exception e) {
                     System.out.println("Other exception: " + e.getMessage());
                     e.printStackTrace();
