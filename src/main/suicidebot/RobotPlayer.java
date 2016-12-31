57eb164206a470847c9a982590b282580130a8f1
James Gilles <jhgilles@mit.edu>
2016-12-30 20:30:26 -0500

    Add suicidebot

--- src/main/suicidebot/RobotPlayer.java
+++ src/main/suicidebot/RobotPlayer.java
@@ -1,5 +1,8 @@
-package examplefuncsplayer;
+package suicidebot;
 import battlecode.common.*;
+import battlecode.world.ObjectInfo;
+
+import java.util.Arrays;
 
 public class RobotPlayer {
     static RobotController rc;
@@ -7,8 +10,7 @@ public class RobotPlayer {
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
      * If this method returns, the robot dies!
-    **/
-    @SuppressWarnings("unused")
+     **/
     public static void run(RobotController rc) throws GameActionException {
         RobotPlayer.rc = rc;
         switch (rc.getType()) {
@@ -23,28 +25,28 @@ public class RobotPlayer {
                 break;
 
         }
-	}
+    }
 
-	static void runGardener() throws GameActionException {
+    static void runGardener() throws GameActionException {
         System.out.println("I'm a gardener!");
         while (true) {
             Direction dir = new Direction((float) Math.random() * 2 * (float) Math.PI);
             if (rc.canBuildRobot(RobotType.SOLDIER, dir) && Math.random() < .01 && rc.isBuildReady()) {
                 rc.buildRobot(RobotType.SOLDIER, dir);
             }
-            moveRandom();
+            moveSuicide();
             Clock.yield();
         }
     }
 
-	static void runArchon() throws GameActionException {
+    static void runArchon() throws GameActionException {
         System.out.println("I'm an archon!");
         while (true) {
             Direction dir = new Direction((float) Math.random() * 2 * (float) Math.PI);
             if (rc.canBuildRobot(RobotType.GARDENER, dir) && Math.random() < .01 && rc.isBuildReady()) {
                 rc.buildRobot(RobotType.GARDENER, dir);
             }
-            moveRandom();
+            moveSuicide();
             Clock.yield();
         }
     }
@@ -59,13 +61,33 @@ public class RobotPlayer {
                     rc.fireSingleShot(rc.getLocation().directionTo(robots[0].location));
                 }
             }
-            moveRandom();
+            moveSuicide();
 
             Clock.yield();
         }
     }
 
-    static void moveRandom() throws GameActionException {
+    static void moveSuicide() throws GameActionException {
+        BulletInfo[] bullets = rc.senseNearbyBullets(100);
+
+        for (BulletInfo bullet : bullets) {
+            Direction dir = rc.getLocation().directionTo(bullet.getLocation());
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+                return;
+            }
+        }
+
+        RobotInfo[] robots = rc.senseNearbyRobots(100,rc.getTeam().opponent());
+
+        for (RobotInfo robot : robots) {
+            Direction dir = rc.getLocation().directionTo(robot.getLocation());
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+                return;
+            }
+        }
+
         Direction dir = new Direction((float)Math.random() * 2 * (float)Math.PI);
         if (rc.canMove(dir)) {
             rc.move(dir);
