93c349c5bd13d7411059f8142f15fc6e9e6f4123
nmccoy <nmccoy@mit.edu>
2017-01-07 01:31:35 -0600

    Changed order of bodytypes to put Bullet last

--- js/battlecode_generated.js
+++ js/battlecode_generated.js
@@ -50,6 +50,18 @@ battlecode.schema.BodyType = {
   SCOUT: 5,
 
   /**
+   * A tree that belongs to a team and produces bullets.
+   */
+  TREE_BULLET: 6,
+
+  /**
+   * A neutral tree.
+   * This is included for convenience; note this value SHALL NOT appear in
+   * a SpawnedBodyTable.
+   */
+  TREE_NEUTRAL: 7,
+
+  /**
    * A bullet that moves in a perfectly straight line.
    * Note: bullet location updates are not sent; a bullet is defined to be
    * in position loc + dt * vel after dt seconds.
@@ -57,19 +69,7 @@ battlecode.schema.BodyType = {
    * of bullets, and we don't need to send position updates.
    * The event stream will say if a bullet has been destroyed.
    */
-  BULLET: 6,
-
-  /**
-   * A tree that belongs to a team and produces bullets.
-   */
-  TREE_BULLET: 7,
-
-  /**
-   * A neutral tree.
-   * This is included for convenience; note this value SHALL NOT appear in
-   * a SpawnedBodyTable.
-   */
-  TREE_NEUTRAL: 8
+  BULLET: 8
 };
 
 /**
