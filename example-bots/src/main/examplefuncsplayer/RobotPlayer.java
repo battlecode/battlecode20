92e8cf4d14114bd0709434374f346c16c3e33d56
Ashley Lin <ashlin@mit.edu>
2019-11-30 00:11:15 -0500

    smol updates

--- example-bots/src/main/examplefuncsplayer/RobotPlayer.java
+++ example-bots/src/main/examplefuncsplayer/RobotPlayer.java
@@ -23,14 +23,14 @@ public strictfp class RobotPlayer {
 
         turnCount = 0;
 
-        System.out.println("I'm a " + rc.getType() + " and I just got created!");
+        //System.out.println("I'm a " + rc.getType() + " and I just got created!");
         while (true) {
             turnCount += 1;
             // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
             try {
                 // Here, we've separated the controls into a different method for each RobotType.
                 // You can add the missing ones or rewrite this into your own control structure.
-                System.out.println("I'm a " + rc.getType() + "! Location " + rc.getLocation());
+                //System.out.println("I'm a " + rc.getType() + "! Location " + rc.getLocation());
                 switch (rc.getType()) {
                     case HQ:                 runHQ();                break;
                     case MINER:              runMiner();             break;
@@ -61,17 +61,17 @@ public strictfp class RobotPlayer {
     static void runMiner() throws GameActionException {
         tryBlockchain();
         tryMove(randomDirection());
-        if (tryMove(randomDirection()))
-            System.out.println("I moved!");
+        // if (tryMove(randomDirection()))
+        //     System.out.println("I moved!");
         // tryBuild(randomSpawnedByMiner(), randomDirection());
         for (Direction dir : directions)
             tryBuild(RobotType.FULFILLMENT_CENTER, dir);
         for (Direction dir : directions)
-            if (tryGive(dir))
-                System.out.println("I gave soup! " + rc.getTeamSoup());
+            tryGive(dir);
+                //System.out.println("I gave soup! " + rc.getTeamSoup());
         for (Direction dir : directions)
-            if (tryMine(dir))
-                System.out.println("I mined soup! " + rc.getSoupCarrying());
+            tryMine(dir);
+                //System.out.println("I mined soup! " + rc.getSoupCarrying());
     }
 
     static void runRefinery() throws GameActionException {
@@ -202,7 +202,7 @@ public strictfp class RobotPlayer {
      * @throws GameActionException
      */
     static boolean tryGive(Direction dir) throws GameActionException {
-        if (rc.isReady() && rc.canGiveSoup(dir)) {
+        if (rc.isReady() && rc.canGiveSoup(dir, rc.getSoupCarrying())) {
             rc.giveSoup(dir, rc.getSoupCarrying());
             return true;
         } else return false;
