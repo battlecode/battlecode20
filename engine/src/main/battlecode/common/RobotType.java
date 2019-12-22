45f27a2d4414052a90040dd61ea8d61d364dbba1
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2019-12-22 04:38:07 -0500

    make cows pollute

--- engine/src/main/battlecode/common/RobotType.java
+++ engine/src/main/battlecode/common/RobotType.java
@@ -67,7 +67,7 @@ public enum RobotType {
      * Cows produce pollution (and they moo).
      * @battlecode.doc.robottype
      */
-    COW                     (null,  0,  0,  0,  2,  35,  0,  0,  0,  0,  0),
+    COW                     (null,  0,  0,  0,  2,  35,  16,  100,  0,  0,  0),
     //                       SS     C   DL  SL  AC  SR  PR  PA  GP  MS  BL
     ;
     
