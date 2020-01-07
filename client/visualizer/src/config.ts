dfeffee18e0c156c17e789b17a92267c530ba0fc
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2020-01-07 03:30:05 -0500

    prepare for 2020.1.0.1

--- client/visualizer/src/config.ts
+++ client/visualizer/src/config.ts
@@ -111,7 +111,7 @@ export enum Mode {
 export function defaults(supplied?: any): Config {
   supplied = supplied || {};
   return {
-    gameVersion: supplied.gameVersion || "2020.1.0.0", //TODO: Change this on each release!
+    gameVersion: supplied.gameVersion || "2020.1.0.1", //TODO: Change this on each release!
     fullscreen: supplied.fullscreen || false,
     width: supplied.width || 600,
     height: supplied.height || 600,
