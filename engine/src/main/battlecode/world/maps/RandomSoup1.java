1ba59d8395e798bd2b4c8a3abbd2ef1f6c4828fd
Ashley Lin <ashlin@mit.edu>
2020-01-08 00:48:02 -0500

    added random map

--- engine/src/main/battlecode/world/maps/RandomSoup1.java
+++ engine/src/main/battlecode/world/maps/RandomSoup1.java
@@ -36,17 +36,33 @@ public class RandomSoup1 {
         MapBuilder mapBuilder = new MapBuilder(mapName, width, height, 148);
         mapBuilder.setWaterLevel(0);
         mapBuilder.setSymmetry(MapBuilder.MapSymmetry.horizontal);
-        mapBuilder.addSymmetricHQ(20, 8);
+        mapBuilder.addSymmetricHQ(18, 6);
 
+        mapBuilder.setSymmetricSoup(2, 4, 130);
+        mapBuilder.setSymmetricSoup(3, 8, 210);
+        mapBuilder.setSymmetricSoup(6, 5, 100);
+        mapBuilder.setSymmetricSoup(6, 19, 150);
+        mapBuilder.setSymmetricSoup(7, 20, 80);
+        mapBuilder.setSymmetricSoup(18, 1, 280);
+        mapBuilder.setSymmetricSoup(19, 5, 60);
+        mapBuilder.setSymmetricSoup(20, 4, 50);
+        mapBuilder.setSymmetricSoup(21, 17, 60);
+        mapBuilder.setSymmetricSoup(24, 7, 130);
+        mapBuilder.setSymmetricSoup(27, 8, 210);
+        mapBuilder.setSymmetricSoup(30, 5, 100);
+        mapBuilder.setSymmetricSoup(34, 20, 80);
+        mapBuilder.setSymmetricSoup(34, 15, 140);
+        mapBuilder.setSymmetricSoup(35, 4, 100);
+        mapBuilder.setSymmetricSoup(39, 25, 280);
+        mapBuilder.setSymmetricSoup(41, 16, 80);
+        mapBuilder.setSymmetricSoup(42, 9, 150);
+        mapBuilder.setSymmetricSoup(44, 1, 280);
+        mapBuilder.setSymmetricSoup(46, 5, 60);
+        mapBuilder.setSymmetricSoup(49, 4, 50);
+        mapBuilder.setSymmetricSoup(52, 17, 60);
 
-        // add some nice central soup
-        addSoup(mapBuilder, 30, 20, 5, 10);
-        // add some team soup
-        addSoup(mapBuilder, 10, 30, 4, 5);
-
-
-        for(int i = 18; i < mapBuilder.width-10; i++) {
-            for (int j = 22; j < mapBuilder.height-10; j++) {
+        for(int i = 11; i < mapBuilder.width-15; i++) {
+            for (int j = 25; j < mapBuilder.height-19; j++) {
                 mapBuilder.setSymmetricWater(i,j,true);
             }
         }
@@ -58,7 +74,7 @@ public class RandomSoup1 {
         }
 
         mapBuilder.addSymmetricCow(5, 18);
-        mapBuilder.addSymmetricCow(17, 21);
+        mapBuilder.addSymmetricCow(17, 3);
 
         mapBuilder.saveMap(outputDirectory);
 
