9a5ff1daa550088369bc67751f8569a639fef066
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2020-01-13 15:03:42 -0500

    prepare for 2020.1.1.0 because maven is weird

--- client/visualizer/src/config.ts
+++ client/visualizer/src/config.ts
@@ -115,7 +115,7 @@ export enum Mode {
 export function defaults(supplied?: any): Config {
   supplied = supplied || {};
   return {
-    gameVersion: supplied.gameVersion || "2020.1.0.10", //TODO: Change this on each release!
+    gameVersion: supplied.gameVersion || "2020.1.1.0", //TODO: Change this on each release!
     fullscreen: supplied.fullscreen || false,
     width: supplied.width || 600,
     height: supplied.height || 600,
