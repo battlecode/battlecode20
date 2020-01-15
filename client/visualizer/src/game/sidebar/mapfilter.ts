0df967b1eadc595b35d3ffdaad692526375090cd
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2020-01-14 22:48:29 -0500

    show sprint maps in client

--- client/visualizer/src/game/sidebar/mapfilter.ts
+++ client/visualizer/src/game/sidebar/mapfilter.ts
@@ -21,7 +21,7 @@ export default class MapFilter {
   private readonly filterType: Map<MapType, HTMLInputElement>;
 
   // Map types available (NOTE: Update after each tournament)
-  private readonly types: MapType[] = [MapType.DEFAULT, MapType.CUSTOM];
+  private readonly types: MapType[] = [MapType.DEFAULT, MapType.CUSTOM, MapType.SPRINT];
 
   // All the maps displayed on the client
   private maps: Array<MapSchema>;
